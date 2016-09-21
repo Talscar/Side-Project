73267d11873d6f6cc307d2c57a66b9660b3b9404
Talscar-PC\Talscar <talscartalcom@gmail.com>
2016-09-21 22:23:26 +0930

    Deformer with some light animation functions for testing purposes, constructor just build to house the list value of itself. To make its own arrary of vert4[] been the numeric value of the vert and its location. May need a lot of refinement and creative testing and crafting to make things work. Seems like a great mechanic to use! Would be good to update the collider as well to deform itself too for future purposes.

--- BodySnatcher Level Proto 0.0.0.1/Assets/unityScript_meshVert.cs
+++ BodySnatcher Level Proto 0.0.0.1/Assets/unityScript_meshVert.cs
@@ -1,26 +1,99 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class unityScript_meshVert : MonoBehaviour {
+    public bool requestPull = true;
+    public bool standardTest = false;
+    public bool complicatedBuild = true;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public float time = 15f;
+    float count = 0.0f;
+    bool switche = true;
+
+    public float movement = 50.0f;
+
+    
+
+	void Start ()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        /*int i = 0;
+      //  int[] vetexCount = vetexCount[i].Length = mesh.vertices.Length;
+        foreach(ContactPoint vert in mesh)
+        {
+            vert4[i] = mesh.vertices.Length;
+        } */
+        {
+                int i = 0;
+                while(i < vertices.Length)
+                {
+
+                if(i == 0)
+                { break; }
+                }
+            } //end point
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        Mesh mesh = GetComponent<MeshFilter>().mesh;
-        Vector3[] vertices = mesh.vertices;
-        int i = 0;
-        while (i < vertices.Length)
+        if(complicatedBuild)
         {
-            vertices[i] += Vector3.up * Time.deltaTime;
-            i++;
+            Mesh mesh = GetComponent<MeshFilter>().mesh;
+            Vector3[] vertices = mesh.vertices;
+
+
+
         }
-        mesh.vertices = vertices;
-        mesh.RecalculateBounds();
+
+        if(standardTest /*&& count < time*/){
+            Mesh mesh = GetComponent<MeshFilter>().mesh;
+            Vector3[] vertices = mesh.vertices;
+            int i = 0;
+            bool a = false;
+            bool b = false;
+            bool c = false;
+            bool d = true;
+            if (requestPull)
+            {
+                while (i < vertices.Length)
+                {
+                    //if
+                    if (a)
+                    { vertices[i].y += movement + movement + 0.09f; a = false; d = true; }
+                    else if (b)
+                    { vertices[i].y += movement - movement + 0.04f; b = false; a = true; }
+                    else if (c)
+                    { vertices[i].y += movement + movement - 0.05f; c = false; b = true; }
+                    else if (d)
+                    { vertices[i].y += movement + movement; /** Time.deltaTime*/ d = false; c = true; }
+                    //vertices[i].y = movement * Time.deltaTime;
+                    i++;
+                }
+                //requestPull = false;
+            }
+
+            if(count >= time)
+            {
+                if(switche)
+                {
+                    switche = false;
+                    count = 0;
+                    movement = 0 - movement; 
+                }
+                if(!switche)
+                {
+                    switche = true;
+                    count = 0;
+                    movement = 0 + movement;
+                }
+            }
+            else { count = count + (1 * Time.deltaTime);}
+            mesh.vertices = vertices;
+            mesh.RecalculateBounds();
+        }
 
     }
 }
