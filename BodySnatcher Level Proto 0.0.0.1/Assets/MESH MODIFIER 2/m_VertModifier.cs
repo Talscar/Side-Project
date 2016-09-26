5b8baccb4befe85afc9931432796cbfed6481cac
Michael Gackle <michael.gackle@ad.aie.edu>
2016-09-26 16:00:55 +0930

    Has some terrible code that needs influencing, null refrence exception don't use this update, to be updated. Master_2 ONLY not main master.

--- BodySnatcher Level Proto 0.0.0.1/Assets/MESH MODIFIER 2/m_VertModifier.cs
+++ BodySnatcher Level Proto 0.0.0.1/Assets/MESH MODIFIER 2/m_VertModifier.cs
@@ -3,8 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class m_VertModifier : MonoBehaviour {
-    public GameObject mesh_ToModify;
+    public bool UsingVertMaxHight = true;
+    Vector3[] MaxHight;
 
+    public GameObject mesh_ToModify;
     public int index_Id = 0;
     public int mod_Location = 0;
     int locate = 0;
@@ -19,21 +21,29 @@ public class m_VertModifier : MonoBehaviour {
 
         Debug.DrawRay(mesh_ToModify.GetComponent<m_VertCount>().vertices[locate] , gameObject.transform.position - new Vector3(gameObject.transform.position.x, 0, gameObject.transform.position.z), Color.red);
 
-///<example        Debug.DrawRay(vertices[LocateAndControlRayTestTemp], new_Object.transform.position, Color.blue); //unknown
-///                Debug.DrawRay(new_Object.transform.position, vertices[LocateAndControlRayTestTemp], Color.red);
-/// ></example>
-/// 
+        ///<example        Debug.DrawRay(vertices[LocateAndControlRayTestTemp], new_Object.transform.position, Color.blue); //unknown
+        ///                Debug.DrawRay(new_Object.transform.position, vertices[LocateAndControlRayTestTemp], Color.red);
+        /// ></example>
+        /// 
 
+        if (UsingVertMaxHight == true)
+        { MaxHight[locate] = mesh_ToModify.GetComponent<m_VertCount>().vertices[locate]; } //null refrence exception;*
         if(gameObject.transform.position != obj_Position)
         {
             mesh_ToModify.GetComponent<m_VertCount>().vertices[locate] = gameObject.transform.position;
 
             Debug.LogWarning("Pos of Vert line 85: " + mesh_ToModify.GetComponent<m_VertCount>().vertices[locate] + ", Compare to other object: " + mesh_ToModify.GetComponent<m_VertCount>().gameObject.transform.position);
 
+            if(UsingVertMaxHight == true && mesh_ToModify.GetComponent<m_VertCount>().vertices[locate].y > MaxHight[locate].y)
+            {
+                mesh_ToModify.GetComponent<m_VertCount>().vertices[locate] = MaxHight[locate];
+            }
+
             //vert4[LocateAndControlRayTestTemp] = vert4(v);
             mesh_ToModify.GetComponent<m_VertCount>().mesh.vertices = mesh_ToModify.GetComponent<m_VertCount>().vertices;
-
             mesh_ToModify.GetComponent<m_VertCount>().mesh.RecalculateBounds();
+            mesh_ToModify.GetComponent<m_VertCount>().mesh.RecalculateNormals();
+            mesh_ToModify.GetComponent<m_VertCount>().mesh.Optimize();
             obj_Position = gameObject.transform.position;
         }
         else { obj_Position = gameObject.transform.position; }
