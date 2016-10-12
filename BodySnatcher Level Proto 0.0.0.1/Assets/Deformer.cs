3861d243e1bb58c803db6435bbc4a19a3fcd4e2d
Michael Gackle <michael.gackle@ad.aie.edu>
2016-10-12 12:32:06 +1030

    New terrrain mod been created with a:
    verticies[i] = transform.TransformPoint(verticies(verticies[i]));

---
+++ BodySnatcher Level Proto 0.0.0.1/Assets/Deformer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Deformer : MonoBehaviour {
+
+    //I will be pushing terrain into the ground, and be limited to rising to the top and no more.
+    //My travel range is on the Y axis and can use my contact points in the mesh to transform the terrain.
+    //I have a limit depending on what vertices are on the inside location of the (Grave Set) << Grave set is to locate deformers, bodys and other things as well as set range. Anything outside of it's range will be made redundent for use in this script.
+
+
+    ///I will function by finding the closest vertices to my position, getting that position and claiming it as my own.
+    ///Once i have collected that position, no other vertices can take that position and modify it, due to a bool check which when true means others know "I can't occupie this space, another already claimed it".
+    ///
+
+
+
+
+	//////////////// Use this for initialization
+	//////////////void Start () {
+	
+	//////////////}
+	
+	//////////////// Update is called once per frame
+	//////////////void Update () {
+	
+	//////////////}
+}
---
+++ BodySnatcher Level Proto 0.0.0.1/Assets/Terrain_Vert4Pos_Creator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Terrain_Vert4Pos_Creator : MonoBehaviour
+{
+    //I will be determining each and every vertice i have in my collection, but will only assign vertices to a collection if the collection is required and is attatched.
+    //This means i will only record verticies locations to the Collection if its both in range and i can tell in what it needs to know within its list/array of infomation.
+
+
+    //<<If transform position != new transform position update the transform mesh of this game object.>>
+
+
+
+    //////////////// Use this for initialization
+    void Start()
+    {
+        Vector3.Distance(vec1, vec2);
+    }
+
+    //////////////// Update is called once per frame
+    void update()
+    {
+
+    }
+}
+
+
+
+
+
+///External notes:
+///Collections will be attached to the Grave set which will hold all the infomation of the range and sizes of graves as well as truncate anything that is not in range to be ignored.
